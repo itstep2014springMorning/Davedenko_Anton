d53eee092c8ea7c7473557e0810bc87da41228a4
salvados <davedenko83@gmail.com>
2014-09-03 14:37:06 +0300

    add shellsort

--- 21.08.2014/Shell&Bubble_Sort/ShellSort.cpp
+++ 21.08.2014/Shell&Bubble_Sort/ShellSort.cpp
@@ -1,4 +1,36 @@
 #include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+
+void shellesort(int *A, int n){
+	int d, i, j, t;
+	for (d = n / 2; d > 0; d /= 2){
+		for (i = d; i < n; i++){
+			for (j = i; j >= d && A[j - d] > A[j]; j -= d){
+
+				t = A[j];
+				A[j] = A[j - d];
+				A[j - d] = t;
+			}
+		}
+	}
+}
 int main(){
-printf("Hello, World!")
+	int i;
+	int const n = 10;
+	int mas[n];//shell
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		mas[i] = rand() % 100;
+	}
+	for (i = 0; i < n; i++){
+		printf(" %d ", mas[i]);
+	}
+	printf("\n\n");
+	shellesort(mas, n);
+	for (i = 0; i < n; i++){
+		printf(" %d ", mas[i]);
+	}
+	printf("\n");
+	return 0;
 }
