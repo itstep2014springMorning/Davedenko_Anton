7704d398e4b970242def9ae03cc22281fee7c862
salvados <davedenko83@gmail.com>
2014-09-03 14:29:58 +0300

    add bubblesort

--- 21.08.2014/Shell&Bubble_Sort/BubbleSort.cpp
+++ 21.08.2014/Shell&Bubble_Sort/BubbleSort.cpp
@@ -1,4 +1,29 @@
 #include <stdio.h>
+void bubblesort(int *A, int N){
+
+}
 int main(){
-printf("Hello, World!")
+int i;
+int A[10] = { 44, 23, 43, 34,15, 18, 20, 84, 29, 100 };
+for (i = 0; i < 10; i++){
+printf("%d ", A[i]);
+}
+printf("\n");
+int j,tmp;
+
+
+for (j = 0; j < 10-1; j++){
+for (i = 0; i < 10-j-1; i++){
+if (A[i]>A[i + 1]){
+tmp = A[i];
+A[i] = A[i + 1];
+A[i + 1]=tmp;
+}
+}
+}
+for (i = 0; i < 10; i++){
+printf("%d ", A[i]);
+}
+printf("\n");
+return 0;
 }
